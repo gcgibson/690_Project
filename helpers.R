b6f38f92b89a123c4c803a42c6c2604dd8fb19dd
gcgibson <gcgibson@gcgibsons-MacBook-Pro.local>
2018-04-26 10:02:07 -0400

    LDL

--- helpers.R
+++ helpers.R
@@ -21,63 +21,6 @@ join_by_id <- function(df1, df2, id= "seqn"){
   inner_join(df1, df2, by = id)
 }
 
-# bivariate models with depressed
-make_bivariate_table <- function(xvar, yvar = "depressed", dat = nhanes){
-  f <- as.formula(paste(yvar,"~",xvar,sep = ""))
-  m <- glm(f, data = dat, family = binomial)
-  mt <- tidy(m)[2,]
-  mt <- mt %>% mutate(OR = exp(estimate)) %>%
-    select(term, B = estimate, "SE(B)" = std.error,
-           "exp(B)" = OR, "p-value" = p.value,-statistic)
-  mt[,-1] <- round(mt[,-1], 4)
-  mt$term <- if_else(mt[5] <= 0.05, paste(mt$term,"*",sep=""),mt$term)
-  mt
-}
-
-# make multiviate table from tidy mlr mod
-make_multivariate_table <- function(xvec){
-  rhs <- paste(xvec, collapse = "+")
-  f2 <- paste("depressed~",rhs,sep="")
-  m <- glm(f2, data = nhanes, family = binomial)
-  mt <- tidy(m)
-  # mt <- mt[2:nrow(mt),]
-  mt <- mt %>% mutate(OR = exp(estimate)) %>%
-    select(term, B = estimate, "SE(B)" = std.error,
-           "exp(B)" = OR, "p-value" = p.value,-statistic)
-  mt[,-1] <- round(mt[,-1], 4)
-  mt$term <- if_else(mt[5] <= 0.05, paste(mt$term,"*",sep=""),mt$term)
-  mt
-}
-
-make_univariate_table <- function(x, dat = nhanes){
-  rhs <- paste("~", x, sep="")
-  tab <- as.vector(xtabs(as.formula(rhs), data = dat))
-  ptab <- as.vector(prop.table(tab))
-  col <- paste(tab, " (", round(ptab*100, 1), ")", sep = "")
-  d <- data.frame(term = x, col[1],col[2])
-  names(d) <- c("term", "0","1")
-  d
-}
-
-
-make_twoway_table <- function(x, y = "depressed", dat = nhanes){
-  f <- as.formula(paste("~",x,"+",y,sep=""))
-  tab <- xtabs(f, data = nhanes)
-  tabdf <- tidy(tab) %>% spread_(y, "Freq")
-  names(tabdf) <- c("value","no","yes")
-  ptab <- round(prop.table(tab,1),3)*100
-  ptabdf <- tidy(ptab) %>% spread(depressed, "Freq")
-  names(ptabdf) <- c("value","nopct","yespct")
-  twoway <- inner_join(tabdf,ptabdf,by = "value") 
-  twoway$xvar <- x
-  twoway %>% select_("xvar","value","yes", "yespct","no","nopct")
-}
-
-
-get_glance <- function(mod, dat, fam) {
-  m <- glm(mod, data = dat, family = fam)
-  glance(m)
-}
 
 # read data from NHANES website
 get_data <- function(survey, letter){
@@ -97,51 +40,8 @@ get_data <- function(survey, letter){
   pfq <- sasxport.get(paste(url,survey,"/PFQ",letter,".XPT",sep=""))
   slq <- sasxport.get(paste(url,survey,"/SLQ",letter,".XPT",sep=""))
   smq <- sasxport.get(paste(url,survey,"/SMQ",letter,".XPT",sep=""))
+  ldl <-  sasxport.get(paste(url,survey,"/TRIGLY",letter,".XPT",sep=""))
   datalist <- list(demo, bmx, bpx, ghb, tchol, alq, bpq, mcq, dpq, 
-                   inq, paq, pfq, slq, smq) # rxq_rx, 
+                   inq, paq, pfq, slq, smq,ldl) # rxq_rx, 
   Reduce(join_by_id, datalist) %>% mutate(survey = survey)
-}
-
-# perform k-fold cross validation
-get_kfold_acc <- function(seed, rhs, y, train, k){
-  
-  # sort data frame
-  row_inds <- 1:nrow(train)
-  set.seed(seed)
-  train_sort <- train[sample(row_inds),]
-  
-  # split into k blocks
-  train_split <- split(train_sort, 1:k)
-  
-  # initialize vector to hold k mse measurements
-  acc <- numeric(k)
-  
-  # for each, create a training set with cases not in the block
-  # also create a test set with cases in the block
-  for(block in 1:k){
-    
-    # get row indices for test set
-    test_inds <- as.numeric(row.names(train_split[[block]]))
-    
-    # get logical indices for training set
-    train_lgl <- !(row_inds %in% test_inds)
-    
-    # extract training set, fit model
-    train <- train_sort[train_lgl,]
-    mod <- glm(paste(y, "~" , rhs, sep = ""), data = train)
-    
-    # extract test set, make predictions using learned model
-    test <- train_split[[block]]
-    probs <- predict(mod, newdata = test)
-    
-    set.seed(k)
-    runifs <- runif(length(probs),0,1)
-    preds <- (probs > runifs)*1
-    
-    # calculate mse for this block
-    acc[block] <- sum(test[y] == preds)/nrow(test)
-  }
-  
-  # return mean mse of the k blocks
-  mean(acc)
 }
--- read_data.R
+++ read_data.R
@@ -1,13 +1,21 @@
 # Project
 source("helpers.R")
 
-url <- "https://wwwn.cdc.gov/Nchs/Nhanes/"
-
-surveys <- c("2007-2008","2009-2010","2011-2012","2013-2014","2015-2016")
-letters <- paste("_",LETTERS[5:9], sep = "")
-
-# stack the surveys together
-nhanes0 <- map2_df(surveys, letters, get_data)
-
-# write raw data csv for 690 project
-write.csv(nhanes0, "nhanes0.csv", row.names = FALSE)
+DOWNLOADED <- FALSE
+if (!DOWNLOADED){
+  url <- "https://wwwn.cdc.gov/Nchs/Nhanes/"
+  
+  surveys <- c("2007-2008","2009-2010","2011-2012")
+  letters <- paste("_",LETTERS[5:7], sep = "")
+  
+  # stack the surveys together
+  nhanes0 <- map2_df(surveys, letters, get_data)
+  
+  # write raw data csv for 690 project
+  write.csv(nhanes0, "nhanes0.csv", row.names = FALSE)
+} else{
+  nhanes0 <- read.csv("/Users/gcgibson/Downloads/nhanes0.csv")
+}
+  
+  
+  
