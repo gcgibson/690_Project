f0b16fa9d6ab37307c846f1a41776a0a559e5f37
jcrm3594 <jon.moyer@gmail.com>
2018-04-26 11:36:41 -0400

    changes to get_data function

--- helpers.R
+++ helpers.R
@@ -24,24 +24,39 @@ join_by_id <- function(df1, df2, id= "seqn"){
 
 # read data from NHANES website
 get_data <- function(survey, letter){
-  demo <- sasxport.get(paste(url,survey,"/DEMO",letter,".XPT",sep=""))
-  bmx <- sasxport.get(paste(url,survey,"/BMX",letter,".XPT",sep=""))
-  bpx <- sasxport.get(paste(url,survey,"/BPX",letter,".XPT",sep=""))
-  ghb <- sasxport.get(paste(url,survey,"/GHB",letter,".XPT",sep=""))
-  tchol <- sasxport.get(paste(url,survey,"/TCHOL",letter,".XPT",sep=""))
+  demo <- sasxport.get(paste(url,survey,"/DEMO",letter,".XPT",sep="")) %>%
+    select(seqn, riagendr, ridageyr, ridreth1, dmdeduc2, dmdmartl, indfmpir)
+  bmx <- sasxport.get(paste(url,survey,"/BMX",letter,".XPT",sep="")) %>%
+    select(seqn, bmxbmi, bmxht, bmxwt, bmxwaist)
+  bpx <- sasxport.get(paste(url,survey,"/BPX",letter,".XPT",sep="")) %>%
+    select(seqn, starts_with("bpxsy"), starts_with("bpxdi"), bpxpuls)
+  ghb <- sasxport.get(paste(url,survey,"/GHB",letter,".XPT",sep="")) %>%
+    select(seqn, lbxgh)
+  hdl <- sasxport.get(paste(url,survey,"/HDL",letter,".XPT",sep="")) %>%
+    select(seqn, lbdhdd)
+  tchol <- sasxport.get(paste(url,survey,"/TCHOL",letter,".XPT",sep="")) %>%
+    select(seqn, lbxtc)
   bpq <- sasxport.get(paste(url,survey,"/BPQ",letter,".XPT",sep=""))
-  alq <- sasxport.get(paste(url,survey,"/ALQ",letter,".XPT",sep=""))
-  mcq <- sasxport.get(paste(url,survey,"/MCQ",letter,".XPT",sep=""))
+  alq <- sasxport.get(paste(url,survey,"/ALQ",letter,".XPT",sep="")) %>%
+    select(seqn, alq130)
+  mcq <- sasxport.get(paste(url,survey,"/MCQ",letter,".XPT",sep="")) %>%
+    select(seqn, mcq160e, mcq160f, mcq220)
   # rxq_rx <- sasxport.get(paste(url,survey,"/RXQ_RX",letter,".XPT",sep=""))
   # we may not need dpq? it's for depression
-  dpq <- sasxport.get(paste(url,survey,"/DPQ",letter,".XPT",sep=""))
+  dpq <- sasxport.get(paste(url,survey,"/DPQ",letter,".XPT",sep="")) %>%
+    select(seqn, dpq010:dpq090)
   inq <- sasxport.get(paste(url,survey,"/INQ",letter,".XPT",sep=""))
-  paq <- sasxport.get(paste(url,survey,"/PAQ",letter,".XPT",sep=""))
-  pfq <- sasxport.get(paste(url,survey,"/PFQ",letter,".XPT",sep=""))
-  slq <- sasxport.get(paste(url,survey,"/SLQ",letter,".XPT",sep=""))
-  smq <- sasxport.get(paste(url,survey,"/SMQ",letter,".XPT",sep=""))
-  ldl <-  sasxport.get(paste(url,survey,"/TRIGLY",letter,".XPT",sep=""))
+  paq <- sasxport.get(paste(url,survey,"/PAQ",letter,".XPT",sep="")) %>%
+    select(seqn, paq620, paq665)
+  pfq <- sasxport.get(paste(url,survey,"/PFQ",letter,".XPT",sep="")) %>%
+    select(seqn, pfq059)
+  slq <- sasxport.get(paste(url,survey,"/SLQ",letter,".XPT",sep="")) %>%
+    select(seqn, slq060)
+  smq <- sasxport.get(paste(url,survey,"/SMQ",letter,".XPT",sep="")) %>%
+    select(seqn, smq020)
+  trigly <-  sasxport.get(paste(url,survey,"/TRIGLY",letter,".XPT",sep="")) %>%
+    select(seqn, lbxtr, lbdldl)
   datalist <- list(demo, bmx, bpx, ghb, tchol, alq, bpq, mcq, dpq, 
-                   inq, paq, pfq, slq, smq,ldl) # rxq_rx, 
+                   inq, paq, pfq, slq, smq, trigly) # rxq_rx, 
   Reduce(join_by_id, datalist) %>% mutate(survey = survey)
 }
--- read_data.R
+++ read_data.R
@@ -5,8 +5,8 @@ DOWNLOADED <- FALSE
 if (!DOWNLOADED){
   url <- "https://wwwn.cdc.gov/Nchs/Nhanes/"
   
-  surveys <- c("2007-2008","2009-2010","2011-2012")
-  letters <- paste("_",LETTERS[5:7], sep = "")
+  surveys <- c("2007-2008","2009-2010","2011-2012","2013-2014")
+  letters <- paste("_",LETTERS[5:8], sep = "")
   
   # stack the surveys together
   nhanes0 <- map2_df(surveys, letters, get_data)
@@ -14,7 +14,7 @@ if (!DOWNLOADED){
   # write raw data csv for 690 project
   write.csv(nhanes0, "nhanes0.csv", row.names = FALSE)
 } else{
-  nhanes0 <- read.csv("/Users/gcgibson/Downloads/nhanes0.csv")
+  nhanes0 <- read.csv("nhanes0.csv")
 }
   
   
